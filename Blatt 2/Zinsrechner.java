e977e20a5e8ee1e46fc44b33ff36203758d1830f
Maximilian Petri <maximilian.petri2000@gmail.com>
2019-10-30 21:57:18 +0100

    added Blatt 2

--- Blatt 2/Zinsrechner.java
+++ Blatt 2/Zinsrechner.java
@@ -1,5 +1,8 @@
 public class Zinsrechner {
 
+    // Danje Petersen 379748
+    // Maximilian Petri 405602
+
     public static void main(String[] args) {
         double startbetrag = SimpleIO.getDouble("Was ist ihr Startbetrag?");
         double zinssatz = SimpleIO.getDouble("Was ist ihr Zinssatz in Prozent?") / 100 + 1;
@@ -9,25 +12,31 @@ public class Zinsrechner {
             double zielbetrag = SimpleIO.getDouble("Was ist ihr Zielbetrag?");
             double betrag = startbetrag;
             int jahre = 0;
+
             while (betrag < zielbetrag) {
                 betrag *= zinssatz;
                 jahre++;
             }
+
             SimpleIO.output(
                     "Es dauert " + jahre + " Jahre bei einem Zinssatz von " + Math.round(zinssatz * 10E5) / 10E5
                             + "%, um von " + startbetrag + " auf den Betrag " + zielbetrag
                             + " zu sparen. Nach dieser Zeit hat man " + Math.round(betrag * 100.0) / 100.0 + ".",
                     "Ergebnis");
+
         } else if (modus.equals("Zeit")) {
-            int jahre = SimpleIO.getInt("Wie viele Jahre sollen berechnet werden?");
             double betrag = startbetrag;
+            int jahre = SimpleIO.getInt("Wie viele Jahre sollen berechnet werden?");
+
             for (int i = 0; i < jahre; i++) {
                 betrag *= zinssatz;
             }
+
             SimpleIO.output(
                     "Bei einem Zinssatz von " + Math.round(zinssatz * 10E5) / 10E5 + "% und einem Startbetrag von "
                             + startbetrag + " hat man nach " + jahre + " Jahren " + betrag + " gespart.",
                     "Ergebnis");
+
         } else {
             SimpleIO.output("Ihre Eingabe war Fehlerhaft!", "FEHLER");
         }
