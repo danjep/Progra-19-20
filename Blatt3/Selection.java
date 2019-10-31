2596d250664757359f39b2b1988e30658b1ce4c8
Danje Laptop <danje.petersen@outlook.com>
2019-10-31 20:39:59 +0100

    Blatt3 alternative version

---
+++ Blatt3/Selection.java
@@ -0,0 +1,21 @@
+public class Selection {
+
+    public static void selection(int[] a) {
+        int min;
+        int border = 0;
+        int temp;
+        while(border < a.length) {
+          min = border;
+          for(int i = border; i < a.length; i++) {
+            if(a[i] < a[min]) {
+              min = i;
+            }
+          }
+          temp = a[border];
+          a[border] = a[min];
+          a[min] = temp;
+          border++;
+        }
+    }
+
+}
