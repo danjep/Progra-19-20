51e8d62cd7f41b091d807c5141455144d04f2c45
Danje Laptop <danje.petersen@outlook.com>
2019-12-15 17:34:20 +0100

    c) teilweise

--- Blatt9/A2/mySets/MyAbstractSet.java
+++ Blatt9/A2/mySets/MyAbstractSet.java
@@ -1,14 +1,33 @@
 package mySets;
 
-import java.util.Set;
+import java.util;
+import java.lang;
 
 abstract class MyAbstractSet<T> implements java.lang.Iterable, java.util.Set {
 
     public MySetIterator iterator() {
-
+        //TODO
     }
 
+    public int size() {
+        //TODO
+    }
     
+    public boolean isEmpty() {
+        //TODO
+    }
+
+    public boolean contains(java.lang.Object x) {
+        //TODO
+    }
+
+    public java.lang.Object[] toArray() throws java.lang.UnsupportedOperationException {
+        throw java.lang.UnsupportedOperationException;
+    }
+
+    public <T> T[] toArray(T[] arr) throws java.lang.UnsupportedOperationException {
+        throw java.lang.UnsupportedOperationException;
+    }
 
 
 } 
