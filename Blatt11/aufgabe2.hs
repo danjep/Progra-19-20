3f2f1227844f1afbb551ec90954ec7edcf48e2e1
Danje Petersen <danje.petersen@rwth-aachen.de>
2020-01-22 19:48:07 +0100

    bis e fertig

--- Blatt11/aufgabe2.hs
+++ Blatt11/aufgabe2.hs
@@ -6,7 +6,7 @@ data BinTree a b =  Blatt b | Node a (BinTree a b) (BinTree a b) deriving Show
 example :: BinTree (Int -> Bool) Char
 example = 
     Node (\x -> x>4)
-        (Node (\x -> x*x==0)
+        (Node (\x -> (x*x==x))
             (Blatt 'g')
             (Node (\x -> x==0)
                 (Blatt 'u')
@@ -22,8 +22,21 @@ example =
 --c)
 countInnerNodes :: BinTree a b -> Int
 countInnerNodes (Blatt b) = 0
-countInnerNodes (Node _ (c) (d)) = 1 + (countInnerNodes c) + (countInnerNodes d)
+countInnerNodes (Node _ c d) = 1 + (countInnerNodes c) + (countInnerNodes d)
 
 
 --d)
-decodeInt
+decodeInt :: BinTree (Int -> Bool) b -> Int -> b
+decodeInt (Blatt b) x = b
+decodeInt (Node a c d) x    | a x == False = decodeInt c x
+                            | a x == True = decodeInt d x
+                            
+
+--e)
+decode :: BinTree (Int -> Bool) b -> [Int] -> [b]
+decode _ [] = []
+decode (Node a c d) (x:xs) = (decodeInt (Node a c d) x) : (decode (Node a c d) xs)
+
+
+--f)
+
