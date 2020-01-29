0993627cf98f6eb5ed81c6c152ded590f96afc9f
Danje <danje.petersen@rwth-aachen.de>
2020-01-29 13:02:08 +0100

    2e hoffentlich fertig

--- Blatt12/aufgabe2.pl
+++ Blatt12/aufgabe2.pl
@@ -9,7 +9,8 @@ asPrologList(cons(X,Y),[Z|A]) :- asPrologList(Y,A).
 
 
 % c)
-flatten([[]],[]).
+flatten([],[]).
+flatten([[]|C],D) :- flatten(C,D).
 flatten([[A|B]|C],[A|D]) :- flatten([B|C],D).
 
 
@@ -17,12 +18,25 @@ flatten([[A|B]|C],[A|D]) :- flatten([B|C],D).
 tree(node(_, [])).
 tree(node(V, [X|XS])) :- tree(X) , tree(node(V, XS)).
 
+/*  node beschreibt einen Knoten im Baum. Dieser hat 2 Attribute: Den zugewiesenen Wert und
+    eine Liste aus anderen nodes, welche die Kinder des aktuellen nodes sind.
+    Sollte tree also ein node gegeben werden ohne Kinder, so endet der Baum dort, 
+    bzw. wird beim nächsten Kind des Elternnodes fortgesetzt.
+    Dies wird in der zweiten Zeile festgelegt. 
+    Wenn ein node mit mehreren Kindern übergeben wird, fährt das Programm mit dem ersten Kind fort,
+    bis dieser Unterbaum durchlaufen ist und macht dann mit den anderen Kindern weiter. 
+*/
 
-% e) hört bisher nach dem ersten kind auf.
-flattenTree(node([],[]),[]).
-flattenTree(node([],[Y|YS]),[ZS]) :- flattenTree(Y,ZS).
-flattenTree(node([X|XS],[Y|YS],[X|ZS])) :- flattenTree(node(XS,[Y|YS]),ZS).
 
+% e)
+flattenTree(X,YS) :- comp(flattenTreet(X),YS).
+
+flattenTreet(node(W,[])) :- W.
+flattenTreet(node(W, [Y|YS])) :- append(append(W,fT(Y),Res),fT(node([],YS),Ergebnis)).
+
+
+comp([],[]).
+comp(X,X).
 
 append([],YS,YS).
 append(XS,[],XS). 
